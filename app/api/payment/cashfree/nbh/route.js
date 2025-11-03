872625ee4d966ee307011b827eb9c07f2a22933f
Ayush Shukla <149079402+ayushshukla8920@users.noreply.github.com>
2025-11-04 01:13:46 +0530

    Update route.js

--- app/api/payment/cashfree/nbh/route.js
+++ app/api/payment/cashfree/nbh/route.js
@@ -17,7 +17,7 @@ export async function OPTIONS() {
 export async function POST(req) {
     try {
         const {uuid,phone} = await req.json();
-        let total = 1;
+        let total = 99;
         const client_id = process.env.ENV === "prod" ? process.env.CF_APIKEY_PROD : process.env.CF_APIKEY_TEST;
         const client_secret = process.env.ENV === "prod" ? process.env.CF_APISECRET_PROD : process.env.CF_APISECRET_TEST;
         const sid = uuid;
